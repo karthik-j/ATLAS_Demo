f906140706b3d742323a269737f54d13e286c5f3
karthik_janarthanan <karthikjanarthanan@gmail.com>
2015-04-13 03:21:17 -0400

    bug fixes

--- www/js/app.js
+++ www/js/app.js
@@ -36,47 +36,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
     templateUrl: "templates/tabs.html"
   })
 
-  // Each tab has its own nav history stack:
-
-  // .state('tab.dash', {
-  //   url: '/dash',
-  //   views: {
-  //     'tab-dash': {
-  //       templateUrl: 'templates/tab-dash.html',
-  //       controller: 'DashCtrl'
-  //     }
-  //   }
-  // })
-
-  // .state('tab.chats', {
-  //     url: '/chats',
-  //     views: {
-  //       'tab-chats': {
-  //         templateUrl: 'templates/tab-chats.html',
-  //         controller: 'ChatsCtrl'
-  //       }
-  //     }
-  //   })
-  //   .state('tab.chat-detail', {
-  //     url: '/chats/:chatId',
-  //     views: {
-  //       'tab-chats': {
-  //         templateUrl: 'templates/chat-detail.html',
-  //         controller: 'ChatDetailCtrl'
-  //       }
-  //     }
-  //   })
-
-  // .state('tab.account', {
-  //   url: '/account',
-  //   views: {
-  //     'tab-account': {
-  //       templateUrl: 'templates/tab-account.html',
-  //       controller: 'AccountCtrl'
-  //     }
-  //   }
-  // });
-
   .state('tab.atlas', {
     url: '/atlas',
     views: {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,22 +1,59 @@
 angular.module('starter.controllers', [])
 
-// .controller('DashCtrl', function($scope) {})
+.controller('AtlasCtrl', function($scope) {
+	$scope.$on("$ionicView.beforeEnter", function() {
+		var canvasObj = document.getElementById("atlasCanvas");
+		var contextObj = canvasObj.getContext("2d");
+		canvasObj.width = window.innerWidth;
+		canvasObj.height = window.innerHeight
+		var centerX = canvasObj.width/4 + 80;
+		var centerY = canvasObj.height/2 - 60;
+		
+		drawCircle(contextObj, centerX, centerY);
+		drawText(contextObj, 'F1', centerX, centerY);
+		
+		var tap = 1;
+		
+		canvasObj.addEventListener('click', function(event) {
+			contextObj.clearRect(0,0,canvasObj.width,canvasObj.height);
 
-.controller('AtlasCtrl', function($scope) {});
+			var x = event.pageX - canvasObj.offsetLeft ;
+        	var y = event.pageY - canvasObj.offsetTop - 80;
+        	
+        	drawCircle(contextObj, centerX, centerY);
+        	drawText(contextObj, 'F1', centerX, centerY);
+        	
+        	drawCircle(contextObj, x, y);
+			drawText(contextObj, 'T'+tap++, x, y);
+			
+			drawLine(contextObj, centerX, centerY, x, y);
+		}, false);
+		
+	});
+});
 
-// .controller('ChatsCtrl', function($scope, Chats) {
-//   $scope.chats = Chats.all();
-//   $scope.remove = function(chat) {
-//     Chats.remove(chat);
-//   }
-// })
+function drawCircle (context, centerX, centerY){
+	context.beginPath();
+	context.arc(centerX, centerY, 25, 0, 2 * Math.PI, false);
+	context.fillStyle = '#DCDCDC';
+	context.fill();
+	context.lineWidth = 2;
+	context.strokeStyle = '#7D7D7D';
+	context.stroke();
+}
 
-// .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-//   $scope.chat = Chats.get($stateParams.chatId);
-// // })
+function drawText(context, text, x, y){
+	context.font = 'bold 25px Arial';
+	context.fillStyle = '#5C5CFF';
+	context.textAlign = 'center';
+	context.textBaseline="middle"; 
+	context.fillText(text, x, y);
+}
 
-// // .controller('AccountCtrl', function($scope) {
-// //   $scope.settings = {
-// //     enableFriends: true
-// //   };
-// });
+function drawLine(context, startX, startY, endX, endY ){
+	context.beginPath();
+	context.moveTo(startX, startY);
+	context.bezierCurveTo(startX, endY, endX, startY, endX, endY);
+	context.strokeStyle = '#7D7D7D';
+	context.stroke();
+}
